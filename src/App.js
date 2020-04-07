1c04215c7522a2d778c3877f1b04a1747e1f5b2d
ajgp8821 <ajgp8821@gmail.com>
2020-04-06 21:24:58 -0300

    Section 10 - Responsive design (Completed)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import { Grid, Col, Row } from 'react-flexbox-grid';
 import LocationList from './components/LocationList';
 import './App.css';
 
@@ -6,12 +11,12 @@ const cities = [
   'Buenos Aires,ar',
   'Caracas,ve',
   'Barquisimeto,ve',
-  // 'Bogota,col',
-  // 'Mexico,mex',
-  // 'Madrid,es',
-  // 'Toronto,ca',
-  // 'Baker Lake,ca',
-  // 'Sidney,au',
+  'Bogota,col',
+  'Mexico,mex',
+  'Madrid,es',
+  'Toronto,ca',
+  'Baker Lake,ca',
+  'Sidney,au',
 ]
 
 class App extends Component {
@@ -22,14 +27,37 @@ class App extends Component {
 
   render(){
     return (
-      <div className="App">
-        {/* <header className="App-header"> */}
-          <LocationList
-            cities={cities}
-            onSelectionLocation={this.handleSelectedLocation}
-          />
-        {/* </header> */}
-      </div>
+      <Grid>
+        <Row>
+          <AppBar position='sticky'>
+            <Toolbar>
+              <Typography variant='title' color='inherit'>
+                Weather App
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Row>
+        <Row>
+          <Col xs={12} md={6}>
+            <LocationList
+              cities={cities}
+              onSelectionLocation={this.handleSelectedLocation}
+            />
+          </Col>
+          <Col xs={12} md={6}>
+            <Paper elevation={4}>
+              <div className="details"></div>
+            </Paper>
+          </Col>
+        </Row>
+      </Grid>
+
+
+
+      // <div className="App">
+      //   <header className="App-header">
+      //   </header>
+      // </div>
     );
   };
 }
--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import WeatherLocation from './WeatherLocation';
+import './styles.css';
 
 const LocationList = ( {cities, onSelectionLocation } ) => {
 
@@ -20,7 +21,7 @@ const LocationList = ( {cities, onSelectionLocation } ) => {
     );
 
     return (
-        <div>
+        <div className="locationList">
             {strToComponents(cities)}
         </div>
     )
